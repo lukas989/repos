c0beed67783e1691bec7d34499ef013b4b3cf6a0
Łukasz Woźniczko <lukaszwz@abcdata.eu>
2019-02-16 15:02:21 +0100

    Update

---
+++ Database/dbo/Tables/DeliveryPlans.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [dbo].[DeliveryPlans]
+(
+[DeliveryPlanId] INT IDENTITY(1,1) NOT NULL,
+[DeliveryPlanStatusId] INT NOT NULL, 
+[CustomerId] INT NOT NULL,
+[ExpectedDate] datetime NULL,
+[EntryAuthor] varchar (32)  NOT NULL  DEFAULT (SUSER_SNAME()),
+[EntryDate] datetime NOT NULL  DEFAULT (GETDATE()),
+[LastAuthor] varchar (32)  NOT NULL  DEFAULT (SUSER_SNAME()),
+[LastUpdate] datetime NOT NULL  DEFAULT (GETDATE()),
+CONSTRAINT [PK_DeliveryPlans] PRIMARY KEY CLUSTERED ([DeliveryPlanId]),
+CONSTRAINT FK_Customer_DeliveryPlans FOREIGN KEY ([CustomerId])  REFERENCES Customers([CustomerId])
+)
