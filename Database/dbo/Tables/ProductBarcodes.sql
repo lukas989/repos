001156b6da598f6447ce9c7fe3c8c7c790c93378
Łukasz Woźniczko <lukaszwz@abcdata.eu>
2019-02-26 23:24:13 +0100

    update sql

---
+++ Database/dbo/Tables/ProductBarcodes.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[ProductBarcodes]
+(
+	[ProductId] INT NOT NULL,
+	[BarCode] [varchar] (50) NOT NULL,
+	[BarCodeType] [varchar] (6) NOT NULL,
+	[EntryAuthor] varchar (32) NOT NULL DEFAULT (SUSER_SNAME()),
+	[EntryDate] datetime NOT NULL DEFAULT (GETDATE()),
+	[LastAuthor] varchar (32) NOT NULL DEFAULT (suser_sname()),
+	[LastUpdate] datetime NOT NULL  DEFAULT (getdate()),
+	CONSTRAINT [PK_ProductBarcodes] PRIMARY KEY CLUSTERED ([ProductId], [BarCode], [BarCodeType]),
+	CONSTRAINT FK_Products_ProductBarcodes FOREIGN KEY ([ProductId])  REFERENCES Products([ProductId]),
+)
