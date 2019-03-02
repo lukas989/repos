e16a41a5808262d7bc990ef448457a41206423c6
Łukasz Woźniczko <lukaszwz@abcdata.eu>
2019-03-02 17:31:52 +0100

    Update init App Suppliers

---
+++ Suppliers/Controllers/SuppliersController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Suppliers;
+
+namespace Suppliers.Controllers
+{
+    public class SuppliersController : Controller
+    {
+        private WmsIntegrationEntities db = new WmsIntegrationEntities();
+
+        // GET: Suppliers
+        public ActionResult Index()
+        {
+            return View(db.Suppliers.ToList());
+        }
+
+        // GET: Suppliers/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Suppliers suppliers = db.Suppliers.Find(id);
+            if (suppliers == null)
+            {
+                return HttpNotFound();
+            }
+            return View(suppliers);
+        }
+
+        // GET: Suppliers/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Suppliers/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "SupplierId,Name,Description,EntryAuthor,EntryDate,LastAuthor,LastUpdate")] Suppliers suppliers)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Suppliers.Add(suppliers);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(suppliers);
+        }
+
+        // GET: Suppliers/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Suppliers suppliers = db.Suppliers.Find(id);
+            if (suppliers == null)
+            {
+                return HttpNotFound();
+            }
+            return View(suppliers);
+        }
+
+        // POST: Suppliers/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "SupplierId,Name,Description,EntryAuthor,EntryDate,LastAuthor,LastUpdate")] Suppliers suppliers)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(suppliers).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(suppliers);
+        }
+
+        // GET: Suppliers/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Suppliers suppliers = db.Suppliers.Find(id);
+            if (suppliers == null)
+            {
+                return HttpNotFound();
+            }
+            return View(suppliers);
+        }
+
+        // POST: Suppliers/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Suppliers suppliers = db.Suppliers.Find(id);
+            db.Suppliers.Remove(suppliers);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
