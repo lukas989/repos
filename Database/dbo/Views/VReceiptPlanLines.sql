7b2233471b783b70f7b06942281b64256636e6a2
Łukasz Woźniczko <lukaszwz@abcdata.eu>
2019-07-26 22:04:56 +0200

    update DB

---
+++ Database/dbo/Views/VReceiptPlanLines.sql
@@ -0,0 +1,18 @@
+﻿CREATE VIEW dbo.VReceiptPlanLines
+AS 
+SELECT rpl.ReceiptPlanId,
+       rpl.ReceiptPlanLineNo,
+	   p.Name AS Product,
+       rpl.ExpectedQty,
+       rpl.RecivedQty,
+       rpl.PurchaseOrderId,
+       rpl.PurchaseOrderLineNo,
+       pol.OrderedQty,
+       pol.RecivedQty AS OrderRecivedQty,
+       rpl.EntryAuthor,
+       rpl.EntryDate,
+       rpl.LastAuthor,
+       rpl.LastUpdate
+FROM dbo.ReceiptPlanLines rpl
+	   LEFT JOIN dbo.PurchaseOrderLines pol ON pol.PurchaseOrderId = rpl.PurchaseOrderId AND pol.PurchaseOrderLineNo = rpl.PurchaseOrderLineNo
+	   LEFT JOIN dbo.Products p ON p.ProductId = pol.ProductId
