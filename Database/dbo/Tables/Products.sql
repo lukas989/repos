8d5996ed8165a27866d6dc9b2c71aa90cbe3cbc8
Łukasz Woźniczko <lukaszwz@abcdata.eu>
2019-08-10 16:26:45 +0200

    update

--- Database/dbo/Tables/Products.sql
+++ Database/dbo/Tables/Products.sql
@@ -1,17 +1,18 @@
-﻿CREATE TABLE [dbo].[Products]
-(
-	[ProductId] INT IDENTITY(1,1) NOT NULL,
-	[SupplierId] INT NOT NULL,
-	[Name] varchar (50) NOT NULL,
-	[Description] varchar (255) NULL,
-	[Weight] [decimal] (18, 2) NULL,
-	[Height] [decimal] (10, 2) NULL,
-	[Width] [decimal] (10, 2) NULL,
-	[Depth] [decimal] (10, 2) NULL,
-	[EntryAuthor] varchar (32) NOT NULL DEFAULT (SUSER_SNAME()),
-	[EntryDate] datetime NOT NULL DEFAULT (GETDATE()),
-	[LastAuthor] varchar (32) NOT NULL DEFAULT (suser_sname()),
-	[LastUpdate] datetime NOT NULL  DEFAULT (getdate()),
-	CONSTRAINT [PK_Products] PRIMARY KEY CLUSTERED ([ProductId]),
-	CONSTRAINT [FK_Supplier_Products] FOREIGN KEY ([SupplierId])  REFERENCES Suppliers([SupplierId])
-)
+﻿CREATE TABLE [dbo].[Products] (
+    [ProductId]   INT             IDENTITY (1, 1) NOT NULL,
+    [SupplierId]  INT             NOT NULL,
+    [Name]        VARCHAR (50)    NOT NULL,
+    [Description] VARCHAR (255)   NULL,
+    [Weight]      DECIMAL (18, 2) NULL,
+    [Height]      DECIMAL (10, 2) NULL,
+    [Width]       DECIMAL (10, 2) NULL,
+    [Depth]       DECIMAL (10, 2) NULL,
+    [EntryAuthor] VARCHAR (32)    DEFAULT (suser_sname()) NOT NULL,
+    [EntryDate]   DATETIME        DEFAULT (getdate()) NOT NULL,
+    [LastAuthor]  VARCHAR (32)    DEFAULT (suser_sname()) NOT NULL,
+    [LastUpdate]  DATETIME        DEFAULT (getdate()) NOT NULL,
+    [WmsUpdate]   DATETIME        NULL,
+    CONSTRAINT [PK_Products] PRIMARY KEY CLUSTERED ([ProductId] ASC),
+    CONSTRAINT [FK_Supplier_Products] FOREIGN KEY ([SupplierId]) REFERENCES [dbo].[Suppliers] ([SupplierId])
+);
+
