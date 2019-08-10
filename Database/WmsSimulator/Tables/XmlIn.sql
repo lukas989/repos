9c55596deea29b2e2ccb8e710106505e5f490c1e
Łukasz Woźniczko <lukaszwz@abcdata.eu>
2019-08-10 13:26:52 +0200

    update

--- Database/WmsSimulator/Tables/XmlIn.sql
+++ Database/WmsSimulator/Tables/XmlIn.sql
@@ -2,6 +2,9 @@
     [Id]        INT            IDENTITY (1, 1) NOT NULL,
     [EntryDate] DATETIME       DEFAULT (getdate()) NOT NULL,
     [Content]   VARCHAR (255)  NULL,
-    [Body]      NVARCHAR (MAX) NULL
+    [Body]      NVARCHAR (MAX) NULL,
+    CONSTRAINT [PK_WmsSimulator_XmlIn] PRIMARY KEY CLUSTERED ([Id] ASC)
 );
+
+
 
--- Database/WmsSimulator/Tables/XmlOut.sql
+++ Database/WmsSimulator/Tables/XmlOut.sql
@@ -2,6 +2,9 @@
     [Id]        INT            IDENTITY (1, 1) NOT NULL,
     [EntryDate] DATETIME       DEFAULT (getdate()) NULL,
     [Content]   VARCHAR (255)  NULL,
-    [Body]      NVARCHAR (MAX) NULL
+    [Body]      NVARCHAR (MAX) NULL,
+    CONSTRAINT [PK_WmsSimulator_XmlOut] PRIMARY KEY CLUSTERED ([Id] ASC)
 );
+
+
 
