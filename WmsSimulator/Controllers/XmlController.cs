25d12a7755554fcf947cc0aff2ce8f8fd6daabd5
Łukasz Woźniczko <lukaszwz@abcdata.eu>
2019-08-10 12:59:44 +0200

    update xml controller

--- WmsSimulator/Controllers/XmlController.cs
+++ WmsSimulator/Controllers/XmlController.cs
@@ -1,19 +1,30 @@
-﻿using System;
+﻿using Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Threading.Tasks;
 using System.Web.Http;
+using System.Web.UI.WebControls;
 
 namespace WmsSimulator.Controllers
 {
     public class XmlController : ApiController
     {
+        private WmsConnectionEntities db = new WmsConnectionEntities();
         // PUT: api/Xml/5
-        public void Put(string content, HttpRequestMessage request)
+        [HttpPut]
+        public HttpResponseMessage Put(string content, HttpRequestMessage request)
         {
-            DateTime currentDate =  DateTime.Now;
 
+            if (String.IsNullOrWhiteSpace(content))
+                return Request.CreateResponse(HttpStatusCode.BadRequest);
+
+            db.Products.Add(products);
+            db.SaveChanges();
+
+            return Request.CreateResponse(HttpStatusCode.OK);
         }
     }
 }
