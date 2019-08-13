350e35e5a9ac07c470546a2192b9fe5afdc285bb
Łukasz Woźniczko <lukaszwz@abcdata.eu>
2019-08-13 19:15:06 +0200

    update model

---
+++ Models/Wms/ReceiptExportHeader.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models.Wms
+{
+    public class ReceiptExportHeader
+    {
+        public string ErCode { get; set; }
+        public string LogisticSite { get; set; }
+        public int ReferenceNumber { get; set; }
+    }
+}
---
+++ Models/Wms/ReceiptExportLine.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models.Wms
+{
+    public class ReceiptExportLine
+    {
+        public string PartNumber { get; set; }
+        public string ExpectedQuantity { get; set; }
+        public string ReceivedQuantity { get; set; }
+    }
+}
