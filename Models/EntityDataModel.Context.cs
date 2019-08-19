4e77484c5709be18afdd4f31e58f44b9e4ce26ff
Łukasz Woźniczko <lukaszwz@abcdata.eu>
2019-08-19 16:21:50 +0200

    update

--- Models/EntityDataModel.Context.cs
+++ Models/EntityDataModel.Context.cs
@@ -18,6 +18,7 @@ namespace Models
         public WmsConnectionEntities()
             : base("name=WmsConnectionEntities")
         {
+            this.Configuration.LazyLoadingEnabled = false;
         }
     
         protected override void OnModelCreating(DbModelBuilder modelBuilder)
