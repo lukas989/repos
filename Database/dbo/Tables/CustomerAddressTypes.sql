aa137377b9669817a16e7e87c55f8484f1c1eb65
Łukasz Woźniczko <lukaszwz@abcdata.eu>
2019-11-23 18:49:26 +0100

    update

--- Database/dbo/Tables/CustomerAddressTypes.sql
+++ Database/dbo/Tables/CustomerAddressTypes.sql
@@ -8,11 +8,3 @@
 
 --CONSTRAINT FK_ReceiptPlanLines_PurchaseOrderLines FOREIGN KEY ([PurchaseOrderId],[PurchaseOrderLineNo])  REFERENCES ReceiptPlanLines([PurchaseOrderId],[PurchaseOrderLineNo])
 
-CREATE TABLE [dbo].[CustomerAddressTypes]
-(
-	[CustomerAddressTypeId] INT IDENTITY(1,1) NOT NULL,
-	[Name] varchar (32) NOT NULL, 
-	[Description] varchar (255) NULL,
-	CONSTRAINT [PK_CustomerAddressTypes] PRIMARY KEY CLUSTERED ([CustomerAddressTypeId]),
-	--CONSTRAINT FK_PurchaseOrder_PurchaseOrderStatus FOREIGN KEY ([PurchaseOrderStatusId])  REFERENCES PurchaseOrders([PurchaseOrderStatusId])
-)
